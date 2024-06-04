3344fbebc9c55b8bcc86d75249e5c9bba5db7261
Pablo Ariel Cerbino <pcerbino@gmail.com>
2024-06-04 19:27:37 -0300

    Add public index

--- public/index.php
+++ public/index.php
@@ -1,3 +1,23 @@
 <?php
 
 declare(strict_types=1);
+
+use Jobberwocky\Jobs\Infrastructure\JobRepositorySQLite;
+use Jobberwocky\Jobs\Application\CreateJobService;
+use Jobberwocky\Jobs\Application\FindJobByKeywordService;
+use Jobberwocky\Jobs\Interfaces\Http\Controller\JobController;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$repository = new JobRepositorySQLite();
+$createJobService = new CreateJobService($repository);
+$findJobService = new FindJobByKeywordService($repository);
+$jobController = new JobController($createJobService, $findJobService);
+
+$app->post('/job', [$jobController, 'createJob']);
+$app->get('/jobs', [$jobController, 'findJob']);
+
+$app->run();
