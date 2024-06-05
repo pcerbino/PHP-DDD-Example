79ff90b5815e09e344f3e53d340ee7d6523ce0ec
Pablo Ariel Cerbino <pcerbino@gmail.com>
2024-06-05 18:14:15 -0300

    Add adapter instances for repositories

--- public/index.php
+++ public/index.php
@@ -2,19 +2,33 @@
 
 declare(strict_types=1);
 
+use Dotenv\Dotenv;
+use Jobberwocky\Jobs\Infrastructure\JobRepositoryJobberWockyAPI;
+use Jobberwocky\Jobs\Infrastructure\JobRepositoryJobberWockyAPIAdapter;
 use Jobberwocky\Jobs\Infrastructure\JobRepositorySQLite;
 use Jobberwocky\Jobs\Application\CreateJobService;
 use Jobberwocky\Jobs\Application\FindJobByKeywordService;
+use Jobberwocky\Jobs\Infrastructure\JobRepositorySQLiteAdapter;
 use Jobberwocky\Jobs\Interfaces\Http\Controller\JobController;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
-$repository = new JobRepositorySQLite();
-$createJobService = new CreateJobService($repository);
-$findJobService = new FindJobByKeywordService($repository);
+$app = AppFactory::create();
+$client = new GuzzleHttp\Client();
+
+$localAdapter = new JobRepositorySQLiteAdapter();
+$localRepository = new JobRepositorySQLite($localAdapter);
+$createJobService = new CreateJobService($localRepository);
+
+$remoteAdapter = new JobRepositoryJobberWockyAPIAdapter();
+$remoteRepository = new JobRepositoryJobberWockyAPI($client, $_ENV['JobberwockyExteneralJobsAPI'], $remoteAdapter);
+
+$findJobService = new FindJobByKeywordService($localRepository, $remoteRepository);
+
 $jobController = new JobController($createJobService, $findJobService);
 
 $app->post('/job', [$jobController, 'createJob']);
