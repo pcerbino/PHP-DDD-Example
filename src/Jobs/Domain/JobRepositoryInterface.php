8bbb6f747da9ff7f7eac01edcde115662252b15c
Pablo Ariel Cerbino <pcerbino@gmail.com>
2024-06-05 18:11:36 -0300

    Type return findByKeyword method

--- src/Jobs/Domain/JobRepositoryInterface.php
+++ src/Jobs/Domain/JobRepositoryInterface.php
@@ -5,5 +5,5 @@ namespace Jobberwocky\Jobs\Domain;
 interface JobRepositoryInterface
 {
     public function create(Job $job): void;
-    public function findByKeyword(string $keyword): array;
+    public function findByKeyword(string $keyword): JobRepositoryAdapterResponse;
 }
