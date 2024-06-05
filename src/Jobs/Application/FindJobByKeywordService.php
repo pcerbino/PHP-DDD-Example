b6d636697e85c55a43065a643cf23ccfcd914601
Pablo Ariel Cerbino <pcerbino@gmail.com>
2024-06-05 18:12:27 -0300

    Implement Remote Repository interface

--- src/Jobs/Application/FindJobByKeywordService.php
+++ src/Jobs/Application/FindJobByKeywordService.php
@@ -7,13 +7,17 @@ use Jobberwocky\Jobs\Domain\JobRepositoryInterface;
 class FindJobByKeywordService
 {
     public function __construct(
-        private readonly JobRepositoryInterface $jobRepository
+        private readonly JobRepositoryInterface $jobRepositoryLocal,
+        private readonly JobRepositoryInterface $jobRepositoryRemote
     ) {
     }
 
     public function execute(string $keyword)
     {
-        $jobs = $this->jobRepository->findByKeyword($keyword);
+        $jobs = array_merge(
+            $this->jobRepositoryLocal->findByKeyword($keyword)->jobs,
+            $this->jobRepositoryRemote->findByKeyword($keyword)->jobs
+        );
         return $jobs;
     }
 }
